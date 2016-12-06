edcc006df3dadd7c996daa9635b5cb17c5be54c6
angular-cli <angular-cli@angular.io>
2016-12-05 23:12:48 -0300

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { VideomakerPage } from './app.po';
+
+describe('videomaker App', function() {
+  let page: VideomakerPage;
+
+  beforeEach(() => {
+    page = new VideomakerPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
